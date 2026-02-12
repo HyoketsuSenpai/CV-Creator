1b3fa95f69e14604936213a1a1be7a51139203fc
HyoketsuSenpai <Youseftela@gmail.com>
2026-02-12 09:59:44 +0300

    small fixes

--- src/components/data/Custom_Section.tsx
+++ src/components/data/Custom_Section.tsx
@@ -2,20 +2,28 @@ import './../../styles/Custom_Section.css';
 import { useState, type ReactNode } from "react";
 
 function CustomSection({heading, children, call}: {heading: string, children: ReactNode, call: ()=>void}){
-    const [editMode, setEditMode] = useState(true);
+    const [editMode, setEditMode] = useState(false);
 
     if(!editMode){
-    return (<section>
-        <h2>{heading}</h2>
-        <button onClick={()=>setEditMode(true)}>Edit</button>
-        </section>);
+    return (
+        <section>
+            <div className="section-header">
+                <h2>{heading}</h2>
+                <button className="edit-btn" onClick={()=>setEditMode(true)}>Edit</button>
+            </div>
+        </section>
+    );
     }
 
-    return (<section>
-        <h2>{heading}</h2>
-        {children}
-        <button onClick={()=>{call(); setEditMode(false)}}>Submit</button>
-    </section>);
+    return (
+        <section>
+            <div className="section-header">
+                <h2>{heading}</h2>
+            </div>
+            {children}
+            <button className="submit-btn" onClick={()=>{call(); setEditMode(false)}}>Submit</button>
+        </section>
+    );
 }
 
 export default CustomSection;
